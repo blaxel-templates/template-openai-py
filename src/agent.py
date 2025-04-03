f4406efc689390e45937db32d572e2410a6526c1
cploujoux <cploujoux@blaxel.ai>
2025-04-03 16:20:55 +0200

    Initialize repo

--- src/agent.py
+++ src/agent.py
@@ -1,33 +1,27 @@
 from typing import AsyncGenerator
 
+from agents import Agent, RawResponsesStreamEvent, Runner, function_tool
 from blaxel.models import bl_model
 from blaxel.tools import bl_tools
-from langchain.tools import tool
-from langchain_core.messages import AIMessageChunk
-from langgraph.prebuilt import create_react_agent
+from openai.types.responses import ResponseTextDeltaEvent
 
 
-@tool
-def weather(city: str) -> str:
+@function_tool()
+async def weather(city: str) -> str:
     """Get the weather in a given city"""
     return f"The weather in {city} is sunny"
 
 async def agent(input: str) -> AsyncGenerator[str, None]:
-    prompt = "You are a helpful assistant that can answer questions and help with tasks."
-    tools = await bl_tools(["blaxel-search"]).to_langchain() + [weather]
-    model = await bl_model("gpt-4o-mini").to_langchain()
-    agent = create_react_agent(model=model, tools=tools, prompt=prompt)
-    messages = {"messages": [("user", input)]}
-    async for chunk in agent.astream(messages, stream_mode=["updates", "messages"]):
-        type_, stream_chunk = chunk
-        # This is to stream the response from the agent, filtering response from tools
-        if type_ == "messages" and len(stream_chunk) > 0 and isinstance(stream_chunk[0], AIMessageChunk):
-            msg = stream_chunk[0]
-            if msg.content:
-                if not msg.tool_calls:
-                   yield msg.content
-        # This to show a call has been made to a tool, usefull if you want to show the tool call in your interface
-        if type_ == "updates":
-            if "tools" in stream_chunk:
-                for msg in stream_chunk["tools"]["messages"]:
-                    yield f"Tool call: {msg.name}\n"
+    tools = await bl_tools(["blaxel-search"]).to_openai() + [weather]
+    model = await bl_model("gpt-4o-mini").to_openai()
+
+    agent = Agent(
+        name="blaxel-agent",
+        model=model,
+        tools=tools,
+        instructions="You are a helpful assistant.",
+    )
+    result = Runner.run_streamed(agent, input)
+    async for event in result.stream_events():
+        if isinstance(event, RawResponsesStreamEvent) and isinstance(event.data, ResponseTextDeltaEvent):
+            yield event.data.delta
